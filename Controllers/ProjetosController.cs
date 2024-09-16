10c1544c55c655aca773cd5ce0604fe812fe6cbf
Gabriel Alves de A. <g.alvesdearaujo06@gmail.com>
2024-09-16 12:53:35 -0300

    Adicionando o restante do CRUD do projeto

--- Controllers/ProjetosController.cs
+++ Controllers/ProjetosController.cs
@@ -1,8 +1,8 @@
+using Exo.WebApi.Models;
+using Exo.WebApi.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Exo.WebApi.Repositories;
 using System;
-using Exo.WebApi.Models;
 using Microsoft.DotNet.Scaffolding.Shared.CodeModifier.CodeChange;
 
 namespace Exo.WebApi.Controllers
@@ -16,10 +16,57 @@ namespace Exo.WebApi.Controllers
         {
             _projetoRepository = projetoRepository;
         }
+
+        //Requisição GET all (lista todos os projetos)
         [HttpGet]
         public IActionResult Listar()
         {
             return Ok(_projetoRepository.Listar());
+        }
+
+        //Requisição POST (Adiciona novo projeto)
+        [HttpPost]
+        public IActionResult Cadastrar(Projeto projeto)
+        {
+            _projetoRepository.Cadastrar(projeto);
+            return StatusCode(201);
+        }
+
+        //Requisição GET by Id (Lista projeto especificado)
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            Projeto projeto = _projetoRepository.BuscarporId(id);
+            if (projeto == null)
+            {
+                return NotFound();
+            }
+            else{
+                return Ok(projeto);
+            }
+        }
+
+        //Requisição PUT (Atualiza informações de projeto especificado na requisição)
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Projeto projeto)
+        {
+            _projetoRepository.Atualizar(id, projeto);
+            return StatusCode(204);
+        }
+
+        //Requisição DELETE (deleta projeto do BD)
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _projetoRepository.Deletar(id);
+                return StatusCode(204);
+            }
+            catch (System.Exception)
+            {
+                return BadRequest();
+            }
         }
     }
 }
--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26b35cf516ae6577117a1ae54aefff9fb4ab94d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
